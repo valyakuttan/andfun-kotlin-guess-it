b514b9e02ee7e5c3598710b8f8d7c3e5d261611a
valyakuttan <valyakuttan@users.noreply.github.com>
2020-05-23 23:55:40 +0530

    step 07 complete

--- app/src/main/java/com/example/android/guesstheword/screens/score/ScoreViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/score/ScoreViewModel.kt
@@ -16,7 +16,34 @@
 
 package com.example.android.guesstheword.screens.score
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
 // TODO (01) Create the ScoreViewModel class and have it take in an integer constructor parameter
-// called finalScore
+//  called finalScore
+    class ScoreViewModel(finalScore: Int): ViewModel() {
+
 // TODO (06) Add a LiveData for the score and the play again event, using the best practices for
-// encapsulation and event handling that you've learned
+//  encapsulation and event handling that you've learned
+    val score: LiveData<Int>
+        get() = _score
+    private val _score = MutableLiveData<Int>()
+
+    val eventPlayAgain: LiveData<Boolean>
+        get() = _eventPlayAgain
+    private val _eventPlayAgain = MutableLiveData<Boolean>()
+
+    init {
+        _score.value = finalScore
+    }
+
+    fun onPlayAgain() {
+        _eventPlayAgain.value = true
+    }
+
+    fun onPlayAgainComplete() {
+        _eventPlayAgain.value = false
+    }
+}
