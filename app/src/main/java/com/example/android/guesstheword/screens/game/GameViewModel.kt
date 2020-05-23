ff4a0a43a8060ec2b7afe2e48a720986babfc65e
valyakuttan <valyakuttan@users.noreply.github.com>
2020-05-23 13:01:45 +0530

    step 03 completed

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -16,6 +16,7 @@
 
 package com.example.android.guesstheword.screens.game
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 
@@ -26,21 +27,24 @@ class GameViewModel : ViewModel() {
 
     // TODO (01) Wrap word and score in MutableLiveData
     // The current word
-    var word = ""
+    var word = MutableLiveData<String>()
 
     // The current score
-    var score = 0
+    var score = MutableLiveData<Int>()
     // TODO (02) Change references to score and word to score.value and word.value and add the
-    // require null safety checks
+    //  required null safety checks
 
     // The list of words - the front of the list is the next word to guess
     private lateinit var wordList: MutableList<String>
 
 
     init {
+        // TODO (03) Initialize score.value to 0
+        word.value = ""
+        score.value = 0
+
         resetList()
         nextWord()
-        // TODO (03) Initialize score.value to 0
     }
 
     /**
@@ -81,19 +85,19 @@ class GameViewModel : ViewModel() {
         if (wordList.isEmpty()) {
             // gameFinished() should happen here
         } else {
-            word = wordList.removeAt(0)
+            word.value = wordList.removeAt(0)
         }
     }
 
     /** Methods for buttons presses **/
 
     fun onSkip() {
-        score--
+        score.value = score.value?.minus(1)
         nextWord()
     }
 
     fun onCorrect() {
-        score++
+        score.value = score.value?.plus(1)
         nextWord()
     }
 }
